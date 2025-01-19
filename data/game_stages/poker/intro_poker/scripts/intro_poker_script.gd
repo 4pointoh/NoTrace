849750931fb61a6dfe4b9cfd835363a2d5bba0e6
Your Name <you@example.com>
2025-01-19 04:44:23 -0500

    latest

--- data/game_stages/poker/intro_poker/scripts/intro_poker_script.gd
+++ data/game_stages/poker/intro_poker/scripts/intro_poker_script.gd
@@ -2,26 +2,34 @@ extends PokerScript
 
 static var alreadyDisplayed = false
 static var alreadyDisplayed2 = false
+static var alreadyDisplayed3 = false
+static var alreadyDisplayed4 = false
+
 
 # could do cumulative values w/ static variables (eg "you bet more than my car this match")
 
 static func evaluate_poker_game(_pokerInfo : PokerInfo) :
 	var updateResult = PokerUpdateActionResult.new()
-	
-	if _pokerInfo.stage == PokerEnums.PokerStage.POST_FLOP && !alreadyDisplayed:
+
+	if _pokerInfo.playerMoney > 200 && !alreadyDisplayed3:
+		alreadyDisplayed3 = true
+		updateResult.dialogueStartKey = 'FIRST_LOSS'
+		updateResult.actionResult = PokerUpdateActionResult.ACTION_RESULTS.START_DIALOGUE
+		updateResult.shouldPausePoker = true
+		updateResult.shouldHidePoker = true
+	elif _pokerInfo.playerMoney > 300 && !alreadyDisplayed4:
+		alreadyDisplayed4 = true
+		updateResult.dialogueStartKey = 'HALF_GONE'
+		updateResult.actionResult = PokerUpdateActionResult.ACTION_RESULTS.START_DIALOGUE
+		updateResult.shouldPausePoker = true
+		updateResult.shouldHidePoker = true
+	elif _pokerInfo.playerMoney >= 350 && !alreadyDisplayed:
 		alreadyDisplayed = true
-		updateResult.dialogueStartKey = 'START_TEST'
+		updateResult.dialogueStartKey = 'ALMOST_OVER'
 		updateResult.actionResult = PokerUpdateActionResult.ACTION_RESULTS.START_DIALOGUE
 		updateResult.shouldPausePoker = true
 		updateResult.shouldHidePoker = true
-	elif _pokerInfo.stage == PokerEnums.PokerStage.DEAL && _pokerInfo.cpuMoney < 200 && !alreadyDisplayed2:
-		alreadyDisplayed2 = true
-		updateResult.dialogueStartKey = 'START_TEST2'
-		updateResult.actionResult = PokerUpdateActionResult.ACTION_RESULTS.START_DIALOGUE
-		updateResult.shouldPausePoker = true
-		updateResult.shouldHidePoker = true
-	else:
-		updateResult.actionResult = PokerUpdateActionResult.ACTION_RESULTS.NOTHING
+
 	
 	return updateResult
 	
